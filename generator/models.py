9789a5784e889a650125836c19c84458075a96fe
Matthew Eng <mattheweng@Matt-53690.local>
2018-12-31 17:12:06 -0500

    Add database to hold responses and make templates show previous responses

--- generator/models.py
+++ generator/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Response(models.Model):
+    date = models.TextField(default="")
+    prompt = models.TextField(default="")
+    response = models.TextField(default="")
--- generator/views.py
+++ generator/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from run import DiscoveryGenerator
+from generator.models import Response
 import datetime as dt
+
 
 # Create your views here.
 def homepage(request):
@@ -13,10 +15,23 @@ def homepage(request):
         today.strftime("%Y")
         )
 
+    today_num = today
+
+    if request.method == 'POST':
+        Response.objects.create(response=request.POST['response_text'],
+        date=today_num,
+        prompt=prompt
+        # response_id=Response.id
+        )
+
+    responses = Response.objects.all()
+
     return render(request, 'home.html',
                 {'prompt':
                     prompt,
                 'today_str':
-                    today_str
+                    today_str,
+                'responses':
+                    responses
                 }
             )
